01a0aa0e6a552ffe83bd5bcf74513947bf41524c
user971038 <227220191+user971038@users.noreply.github.com>
2026-09-13 01:50:07 -0600

    moviendo elementos y así

--- flowrider/src/App.tsx
+++ flowrider/src/App.tsx
@@ -1,16 +1,16 @@
 import './App.css';
 
 // Component imports
-import { Titulo } from './Titulo';
-import { Parrafo } from './Parrafo';
-import { Boton } from './Boton';
-import { Documento } from './Documento';
-import { BarraProgreso } from './BarraProgreso';
-import { Carrusel } from './Carrusel';
-import { Grafica } from './Grafica';
-import { Imagen } from './Imagen';
-import { ListaFiltro } from './ListaFiltro';
-import { ListaSimple } from './ListaSimple';
+import { Titulo } from './components/Titulo';
+import { Parrafo } from './components/Parrafo';
+import { Boton } from './components/Boton';
+import { Documento } from './components/Documento';
+import { BarraProgreso } from './components/BarraProgreso';
+import { Carrusel } from './components/Carrusel';
+import { Grafica } from './components/Grafica';
+import { Imagen } from './components/Imagen';
+import { ListaFiltro } from './components/ListaFiltro';
+import { ListaSimple } from './components/ListaSimple';
 
 export default function App() {
   // Mock data for Carrusel
